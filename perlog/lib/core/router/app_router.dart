1dc882d69fa04ed961d83380dffe20a6bb3c7f79
i-chaeyeon <leechae0605@naver.com>
2026-01-30 13:32:29 +0900

    feat: routing 정의

---
+++ perlog/lib/core/router/app_router.dart
@@ -0,0 +1,66 @@
+import 'package:go_router/go_router.dart';
+import 'package:perlog/core/constants/spacing_test_page.dart';
+import 'package:perlog/features/home/home_shell.dart';
+import 'package:perlog/features/onboarding/pages/kakao_login_page.dart';
+import 'package:perlog/features/onboarding/pages/onboarding_profile_page.dart';
+import 'package:perlog/features/onboarding/pages/pin_confirm_page.dart';
+import 'package:perlog/features/onboarding/pages/pin_set_page.dart';
+import 'package:perlog/features/splash/splash_page.dart';
+import 'routes.dart';
+
+bool isOnboarded = true;
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: Routes.paddingTest,
+  routes: [
+    /// Splash
+    GoRoute(
+      path: Routes.splash,
+      builder: (_, __) => const SplashPage(),
+    ),
+
+    /// Kakao Login
+    GoRoute(
+      path: Routes.login,
+      builder: (_, __) => const KakaoLoginPage(),
+    ),
+
+    /// Onboarding Flow
+    GoRoute(
+      path: Routes.onboarding,
+      redirect: (_, __) => '${Routes.onboarding}/${Routes.profile}',
+      routes: [
+        /// 이미지 / 별명 / 알림 / 잠금
+        GoRoute(
+          path: Routes.profile,
+          builder: (_, __) => const OnboardingProfilePage(),
+        ),
+
+        /// PIN 설정
+        GoRoute(
+          path: Routes.pinSet,
+          builder: (_, __) => const PinSetPage(),
+        ),
+
+        /// PIN 확인
+        GoRoute(
+          path: Routes.pinConfirm,
+          builder: (_, __) => const PinConfirmPage(),
+        ),
+      ],
+    ),
+
+    /// Onboarding 완료 후 진입
+    GoRoute(
+      path: Routes.shell,
+      builder: (_, __) => const HomeShell(),
+    ),
+
+
+    ////// debug
+    GoRoute(
+    path: Routes.paddingTest,
+    builder: (_, __) => const PaddingTestPage(),
+    ),
+  ],
+);
---
+++ perlog/lib/core/router/routes.dart
@@ -0,0 +1,16 @@
+class Routes {
+  static const splash = '/';
+  static const login = '/login';
+
+  static const onboarding = '/onboarding';
+  static const profile = 'profile';
+  static const pinSet = 'pin-set';
+  static const pinConfirm = 'pin-confirm';
+
+  static const shell = '/shell';
+
+  
+
+  // debug
+  static const paddingTest = '/padding-test';
+}
