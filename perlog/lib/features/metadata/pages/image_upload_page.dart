6f436814e89371deca3962866a860e8850c44a08
dk <dk7518@naver.com>
2026-02-04 14:09:22 +0900

    feat : image upload page 수정

--- perlog/lib/features/metadata/pages/image_upload_page.dart
+++ perlog/lib/features/metadata/pages/image_upload_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:perlog/core/constants/colors.dart';
-import 'package:perlog/core/router/routes.dart';
-import 'package:flutter/material.dart';
 import 'package:perlog/core/constants/text_styles.dart';
+import 'package:perlog/features/metadata/widgets/back_button.dart';
+import 'package:perlog/core/router/routes.dart';
+import 'package:go_router/go_router.dart';
 
 class ImageUpload extends StatelessWidget {
   const ImageUpload({super.key});
@@ -11,49 +11,22 @@ class ImageUpload extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: TextButton(
-          onPressed: () => context.go(Routes.home),
-          child: const Text('이전'),
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            MetadataBackButton(onTap: () => context.go(Routes.home)),
+            Center(
+              child: Text(
+                'MyAnalysis page',
+                style: AppTextStyles.headline50.copyWith(
+                  color: AppColors.mainFont,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-//
-// class ImageUpload extends StatelessWidget {
-//   const ImageUpload({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     bool lockEnabled = true;
-//
-//     return Scaffold(
-//       backgroundColor: AppColors.background,
-//       body: SafeArea(
-//         child: Stack(
-//           children: [
-//             Column(
-//               children: [
-//                 Text('Calendar page-날짜 선택'),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     if (lockEnabled) {
-//                       context.go(
-//                         '${Routes.metadata}/${Routes.imageUploadFinished}',
-//                       );
-//                     } else {
-//                       context.go(Routes.shell);
-//                     }
-//                   },
-//                   child: const Text('다음'),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
