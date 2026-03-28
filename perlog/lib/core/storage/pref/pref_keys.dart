c04e65209dd2df1ad6276306b6cef0149ff1f986
i-chaeyeon <leechae0605@naver.com>
2026-03-28 12:59:15 +0900

    feat: sharedpreferences 서비스 단 구현 (타입 별 API)
    - 로직 연결 필요

---
+++ perlog/lib/core/storage/pref/pref_keys.dart
@@ -0,0 +1,11 @@
+class PrefKeys {
+  static const nickname = 'nickname';
+
+  static const alarmEnabled = 'alarm_enabled';
+  static const alarmInterval = 'alarm_interval';
+
+  static const lockEnabled = 'lock_enabled';
+  static const pinHash = 'pin_hash';
+
+  static const diaryFont = 'diary_font';
+}
