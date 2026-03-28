0dd702c86521afe3fcc24055a147bf7eba2d3ae1
i-chaeyeon <leechae0605@naver.com>
2026-03-28 12:44:58 +0900

    feat: 색상 변환 함수 구현

---
+++ perlog/lib/core/utils/hex_to_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color hexToColor(String hex) {
+  final buffer = StringBuffer();
+
+  if (hex.length == 7) {
+    buffer.write('ff');
+  }
+
+  buffer.write(hex.replaceFirst('#', ''));
+  return Color(int.parse(buffer.toString(), radix: 16));
+}
