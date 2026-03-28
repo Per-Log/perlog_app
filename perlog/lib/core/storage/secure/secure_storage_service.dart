6126a2acf18eb7e12f51e6c2f000580b03357f01
i-chaeyeon <leechae0605@naver.com>
2026-03-28 13:17:23 +0900

    feat: secure storage 내 로그인 관련 함수 추가

--- perlog/lib/core/storage/secure/secure_storage_service.dart
+++ perlog/lib/core/storage/secure/secure_storage_service.dart
@@ -3,7 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SecureStorageService {
   SecureStorageService._();
 
-  static final _storage = FlutterSecureStorage();
+  static const _storage = FlutterSecureStorage();
 
   static Future<void> write(String key, String value) async {
     await _storage.write(key: key, value: value);
@@ -15,5 +15,47 @@ class SecureStorageService {
 
   static Future<void> delete(String key) async {
     await _storage.delete(key: key);
+  }
+
+  static Future<void> clear() async {
+    await _storage.deleteAll();
+  }
+
+  static const _accessTokenKey = 'access_token';
+
+  static Future<void> setAccessToken(String token) async {
+    await write(_accessTokenKey, token);
+  }
+
+  static Future<String?> getAccessToken() async {
+    return await read(_accessTokenKey);
+  }
+
+  static Future<void> deleteAccessToken() async {
+    await delete(_accessTokenKey);
+  }
+
+  static const _refreshTokenKey = 'refresh_token';
+
+  static Future<void> setRefreshToken(String token) async {
+    await write(_refreshTokenKey, token);
+  }
+
+  static Future<String?> getRefreshToken() async {
+    return await read(_refreshTokenKey);
+  }
+
+  static const _pinKey = 'pin_hash';
+
+  static Future<void> setPinHash(String hash) async {
+    await write(_pinKey, hash);
+  }
+
+  static Future<String?> getPinHash() async {
+    return await read(_pinKey);
+  }
+
+  static Future<void> deletePin() async {
+    await delete(_pinKey);
   }
 }
