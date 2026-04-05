42657728b39a8bae61458cdfb83d91c5fc1a6683
i-chaeyeon <leechae0605@naver.com>
2026-04-05 21:43:48 +0900

    feat: 카카오 sdk 초기화 및 supabase 변수 분리

--- perlog/lib/main.dart
+++ perlog/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:perlog/core/router/app_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -7,13 +9,19 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {  // 2. 비동기 처리를 위해 플러터 엔진 초기화 보장
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
 
   // 3. 한국어(ko_KR) 날짜 데이터가 로드될 때까지 기다림
   await initializeDateFormatting('ko_KR', null);
 
   await Supabase.initialize(
-    url: 'https://uzpfybxalroyynjhbfis.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6cGZ5YnhhbHJveXluamhiZmlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NDM0OTYsImV4cCI6MjA4NzIxOTQ5Nn0.Sx4tsDJe30WNQz-u4QLoI4nwRrRtpJp9WDLVbQEK12s',
+    url: 'SUPABASE_URL',
+    anonKey: 'SUPABASE_ANON_KEY',
+  );
+
+  KakaoSdk.init(
+    nativeAppKey: 'KAKAO_NATIVE_APP_KEY',
   );
 
   runApp(const MyApp());
